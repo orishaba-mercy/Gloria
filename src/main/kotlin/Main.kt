f57af25e020bf4ac0eb9b8c2de221437a45b69b8
orishaba-mercy <orishabamercybanga@gmail.com>
2023-02-06 09:31:35 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main (){
+    var name="bridget"
+    println(name)
+    var friends = arrayOf("dora","dan","hope")
+    println(friends[1])
+
+val hello ="hello"
+    println(hello)
+    var friendsage = arrayOf(23,25,27)
+    println(friendsage[2])
+    var mercyiskenyan  =false
+    println(mercyiskenyan)
+}
+
